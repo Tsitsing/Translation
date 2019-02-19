8218b8986af6035b605f04001a3a19adf6b03570
Tsitsing <liuyusuihua@163.com>
2019-02-20 00:31:19 +0800

    info

--- app/src/main/java/com/tsitsing/translation/MainActivity.java
+++ app/src/main/java/com/tsitsing/translation/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private PictureFragment pictureFragment;
     private DictionaryFragment dictionaryFragment;
     private MyFragment myFragment;
+    private RadioGroup navRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initHomeFragment();//开启时默认的fragment
+        navRadioGroup = findViewById(R.id.nav_radio_group);
+        navRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()){
+                    case R.id.nav_home:
+                        initHomeFragment();
+                        break;
+                    case R.id.nav_voice:
+                        initVoiceFragment();
+                        break;
+                    case R.id.nav_picture:
+                        initPictureFragment();
+                        break;
+                    case R.id.nav_dictionary:
+                        initDictionaryFragment();
+                        break;
+                    case R.id.nav_my:
+                        initMyFragment();
+                        break;
+                }
+            }
+        });
     }
+
 
     //代码冗余，需要改
     private void initHomeFragment(){
@@ -30,21 +58,21 @@ public class MainActivity extends AppCompatActivity {
         if(homeFragment ==null){
             homeFragment = new HomeFragment();
             transaction.add(R.id.nav_fragment_layout,homeFragment);
-            hideFragment(transaction);
-            transaction.show(homeFragment);
-            transaction.commit();
         }
+        hideFragment(transaction);
+        transaction.show(homeFragment);
+        transaction.commit();
     }
 
     private void initVoiceFragment(){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        if(voiceFragment ==null){
+        if(voiceFragment == null){
             voiceFragment = new VoiceFragment();
             transaction.add(R.id.nav_fragment_layout,voiceFragment);
-            hideFragment(transaction);
-            transaction.show(voiceFragment);
-            transaction.commit();
         }
+        hideFragment(transaction);
+        transaction.show(voiceFragment);
+        transaction.commit();
     }
 
     private void initPictureFragment(){
@@ -52,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
         if(pictureFragment ==null){
             pictureFragment = new PictureFragment();
             transaction.add(R.id.nav_fragment_layout,pictureFragment);
-            hideFragment(transaction);
-            transaction.show(pictureFragment);
-            transaction.commit();
         }
+        hideFragment(transaction);
+        transaction.show(pictureFragment);
+        transaction.commit();
     }
 
     private void initDictionaryFragment(){
@@ -63,10 +91,10 @@ public class MainActivity extends AppCompatActivity {
         if(dictionaryFragment ==null){
             dictionaryFragment = new DictionaryFragment();
             transaction.add(R.id.nav_fragment_layout,dictionaryFragment);
-            hideFragment(transaction);
-            transaction.show(dictionaryFragment);
-            transaction.commit();
         }
+        hideFragment(transaction);
+        transaction.show(dictionaryFragment);
+        transaction.commit();
     }
 
     private void initMyFragment(){
@@ -74,14 +102,14 @@ public class MainActivity extends AppCompatActivity {
         if(myFragment ==null){
             myFragment = new MyFragment();
             transaction.add(R.id.nav_fragment_layout,myFragment);
-            hideFragment(transaction);
-            transaction.show(myFragment);
-            transaction.commit();
         }
+        hideFragment(transaction);
+        transaction.show(myFragment);
+        transaction.commit();
     }
 
     private void hideFragment(FragmentTransaction transaction){
-        if (homeFragment !=null){
+        if (homeFragment != null){
             transaction.hide(homeFragment);
         }
         if (voiceFragment != null){
