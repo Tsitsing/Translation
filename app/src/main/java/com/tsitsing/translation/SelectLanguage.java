888a4645646960ad31598d9810d4849848c67be0
Tsitsing <liuyusuihua@163.com>
2019-03-09 13:00:06 +0800

    info

---
+++ app/src/main/java/com/tsitsing/translation/SelectLanguage.java
@@ -0,0 +1,32 @@
+package com.tsitsing.translation;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SelectLanguage {
+    public String getLangCode(String language){
+        String langCode = "auto";
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("自动", "auto");
+            jsonObject.put("中文", "zh");
+            jsonObject.put("繁体", "cht");
+            jsonObject.put("英语", "en");
+            jsonObject.put("日语", "jp");
+            jsonObject.put("韩语", "kor");
+            jsonObject.put("法语", "fra");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            langCode = jsonObject.getString(language);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return langCode;
+    }
+}
